aee6b4bf584343d4a5537ee8f274ffeec3c70ac4
Guillaume Orsal <guillaume@orsal.net>
2015-01-02 19:04:49 +0100

    Update of DB

--- sql/mysql.sql
+++ sql/mysql.sql
@@ -28,10 +28,10 @@ CREATE TABLE IF NOT EXISTS `users` (
  */
 CREATE TABLE IF NOT EXISTS `users_infos` (
   `user_id` INT(11) UNSIGNED NOT NULL,
-  `user_description` TEXT CHARACTER SET utf8 DEFAULT NULL,
-  `user_location` VARCHAR(255) CHARACTER SET utf8 DEFAULT NULL,
-  `user_website` VARCHAR(255) CHARACTER SET utf8 DEFAULT NULL,
-  `user_birthdate` date DEFAULT NULL,
-  `user_last_connexion` timestamp DEFAULT CURRENT_TIMESTAMP,
+  `description` TEXT CHARACTER SET utf8 DEFAULT NULL,
+  `location` VARCHAR(255) CHARACTER SET utf8 DEFAULT NULL,
+  `website` VARCHAR(255) CHARACTER SET utf8 DEFAULT NULL,
+  `birthdate` date DEFAULT NULL,
+  `last_connexion` timestamp DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 ;
