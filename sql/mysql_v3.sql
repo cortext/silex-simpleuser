011edd4fb818780f38996c4876739ad2618070d5
Guillaume Orsal <guillaume@orsal.net>
2015-07-06 16:23:49 +0200

    Ajout de la nouvelle structure de base de donnée

---
+++ sql/mysql_v3.sql
@@ -0,0 +1,9 @@
+/**
+ * Ajout des nouveaux champs utilisateurs
+ */
+
+ALTER TABLE  `users_infos` ADD  `city` VARCHAR( 250 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL ,
+ADD  `country` VARCHAR( 250 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL ,
+ADD  `institution` VARCHAR( 500 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL ,
+ADD  `activity_domain` VARCHAR( 500 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL ,
+ADD  `research_domain` VARCHAR( 500 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL;
