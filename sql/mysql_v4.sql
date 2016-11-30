be806d677e65083831b9f17d8c2cfffc4e689306
Philippe Breucker <breucker@ifris.org>
2016-11-30 14:10:41 +0100

    fix the mysql migration file

--- sql/mysql_v4.sql
+++ sql/mysql_v4.sql
@@ -1,17 +1,6 @@
 /**
- * Ajout des nouveaux champs utilisateurs
+ *  Added authorizations field for User
  */
 
-ALTER TABLE  `users_infos` ADD  `city` VARCHAR( 250 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL ,
-ADD  `country` VARCHAR( 250 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL ,
-ADD  `institution` VARCHAR( 500 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL ,
-ADD  `activity_domain` VARCHAR( 500 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL ,
-ADD  `research_domain` VARCHAR( 500 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
-ADD  `authorizations` text  CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'json formatted list of authorization, for exemple URI that the user can access';
-
-/**
- * Suppression du champ location
- */
-
-ALTER TABLE `users_infos`
-  DROP `location`;
+ALTER TABLE  `users_infos` 
+ADD  `authorizations` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'json formatted list of authorization, for exemple URI that the user can access';
